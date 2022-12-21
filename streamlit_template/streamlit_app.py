7333087ae55f1f3255f2d68fc920e1de17cea392
Selim Rokbani <mohamed.selim.rokbani@umontreal.ca>
2022-12-21 14:59:46 -0500

    Update streamlit_app.py
    Added Client to Streamlit

--- streamlit_template/streamlit_app.py
+++ streamlit_template/streamlit_app.py
@@ -3,47 +3,140 @@ import pandas as pd
 import numpy as np
 import requests
 import json
+import datetime
+import sys
+from ift6758.client import game_client, serving_client
+
+
+########################################
+# Partie client
+# For local testing
+pinger = game_client.GameClient()
+serving = serving_client.ServingClient(ip="127.0.0.1", port=5000)
+
+# For Docker 
+#pinger = game_client.GameClient()
+#serving = serving_client.ServingClient(ip="serving",port=5000)
+
+
+
 
 
 st.title("Hockey visualization App")
-with st.form(key='Form1'):
+
+with st.form(key='Form2'):
    
     with st.sidebar:
         # TODO: Add input for the sidebar
-
-        workspace = st.sidebar.markdown("Workspace")
-        Model = st.sidebar.markdown("Model")
-        Version = st.sidebar.markdown("Version")
+        workspace1 = st.selectbox('Workspace', ('ift6758-22-milestone-2','test'))
+        Model1 = st.selectbox('Model', ('question-6-random-forest-classifier-base','xgboost-task5-model'))
+        Version1 = st.selectbox('Version', ('1.0.0','test'))
         
-        getmodel = st.form_submit_button(label='Get Models')
+        getmodel1 = st.form_submit_button(label='Get Models')
         pass
 
 with st.container():
-    form = st.form(key='GameID')
-    gameid = form.number_input('Enter GameID', step=None, value=0)
-    submit_button = form.form_submit_button(label='Ping game')
-    # gameid = st.text_input('GameID')
+    form1 = st.form(key='GameID')
+    gameid1 = form1.number_input('Enter GameID', step=None, value=2021020312)
+    submit_button1 = form1.form_submit_button(label='Ping games')
+        
+ 
+pass
 
-    if submit_button:
-    # if st.button('Ping game'):
+if getmodel1:
+            #serving.download_registry_model('ift6758-22-milestone-2', 'question-6-random-forest-classifier-base', '1.0.0')
+            serving.download_registry_model(workspace1,Model1,Version1)
+            st.write("Model ",Version1, " Version ",Model1," Workspace ",workspace1," has been uploaded")
 
-        data = requests.get('https://statsapi.web.nhl.com/api/v1/game/{}/feed/live/'.format(gameid)).json()
-        print(data["liveData"]["plays"]["allPlays"])
-        period_no = data["liveData"]["plays"]["allPlays"][-1]["about"]["period"]
-        periodTimeRemaining = data["liveData"]["plays"]["allPlays"][-1]["about"]["periodTimeRemaining"]
-        # td_home = data["liveData"]["boxscore"]["teams"]["home"]["teamStats"]["teamSkaterStats"]
-        home = data["gameData"]["teams"]["home"]["name"]
-        away = data["gameData"]["teams"]["away"]["name"]
-        st.subheader(f'Game {gameid}: {away} @ {home}')
-        st.write(f'Period {period_no} - {periodTimeRemaining} left')
+
+
+
+
+if submit_button1:
+        
+        data = requests.get('https://statsapi.web.nhl.com/api/v1/game/{}/feed/live/'.format(gameid1)).json()
+        game=(pinger.ping(gameid1))
+        period1 = game["period"].iloc[-1]
+        s = game.iloc[-1]["game_elapsed_time"]
+        
+        timet1 = str(datetime.timedelta(seconds=int(s)))
+        
+        #timet1 = str(game.iloc[-1]["game_period_seconds"]).split(":")
+
+        
+       # print(data["liveData"]["plays"]["allPlays"])
+       # period_no = data["liveData"]["plays"]["allPlays"][-1]["about"]["period"]
+       # periodTimeRemaining = data["liveData"]["plays"]["allPlays"][-1]["about"]["periodTimeRemaining"]
+       # # td_home = data["liveData"]["boxscore"]["teams"]["home"]["teamStats"]["teamSkaterStats"]
+       # home = data["gameData"]["teams"]["home"]["name"]
+       # away = data["gameData"]["teams"]["away"]["name"]
+       # st.subheader(f'Game {gameid1}: {away} @ {home}')
+       # st.write(f'Period {period_no} - {periodTimeRemaining} left')
+       # actual_goals = {
+       #    'home': data["liveData"]["plays"]["allPlays"][-1]["about"]["goals"]["home"],
+       #    'away': data["liveData"]["plays"]["allPlays"][-1]["about"]["goals"]["away"],
+       #                }
+        
+        timeLeftt1 = data["liveData"]["plays"]["allPlays"][-1]["about"]["periodTimeRemaining"]
+
+        # get Team Names
+        team1 = data["gameData"]["teams"]["home"]["name"]
+        team2 = data["gameData"]["teams"]["away"]["name"]
+
+        # Separate dataframes for each team
+        gamet1= game[game['team_name']==team1]
+        gamet2= game[game['team_name']==team2]
+        
+        st.subheader(f'Game {gameid1}: {team1} @ {team2}')
+        st.write(f'Period {period1} - {timeLeftt1} left')
         actual_goals = {
             'home': data["liveData"]["plays"]["allPlays"][-1]["about"]["goals"]["home"],
             'away': data["liveData"]["plays"]["allPlays"][-1]["about"]["goals"]["away"],
         }
+        
+        #  Corresponding XG for each teams
+        XGt1 = gamet1[['goal_probability']]
+        XGt2 = gamet2[['goal_probability']]
+        # Sum
+        sumt1 = int(XGt1.sum())
+        sumt2 = int(XGt2.sum())
+        
+        # Dataframe with only goals for each team
+        Gt1 = gamet1[gamet1['event_type']=='GOAL']
+        Gt2 = gamet2[gamet2['event_type']=='GOAL']
+        # Sum of goals for each team
+        goalst1 = int(len(Gt1.index))
+        goalst2 = int(len(Gt2.index))
 
-        # TODO get data from client API
-        #  - Predictions
-        #  - Features
+      # st.subheader('Features data and predictions')
+      # col1, col2 = st.columns(2)
+      # with col1:
+      #     st.metric(
+      #         label=f"{home} - Expected Goals (actual)",
+      #         value=f"{pred_goals['home']} ({actual_goals['home']})",
+      #         delta=pred_goals['home']-actual_goals['home']
+      #     )
+      # with col2:
+      #     st.metric(
+      #     label=f"{away} - Expected Goals (actual)",
+      #         value=f"{pred_goals['away']} ({actual_goals['away']})",
+      #         delta=pred_goals['away']-actual_goals['away']
+      #     )
+
+    # Probleme: les stats ne se reinstalise pas apres chaque ping. 
+        st.subheader('Features data and predictions')
+        col1, col2 = st.columns(2)
+        col1.metric(
+               label=f"{team1} - Expected Goals (actual)",
+               value=f"{sumt1} ({goalst1})",
+               delta=sumt1-goalst1
+           )
+        with col2:
+             st.metric(
+                 label=f"{team2} - Expected Goals (actual)",
+                 value=f"{sumt2} ({goalst2})",
+                 delta=sumt2-goalst2
+             )
         events = pd.DataFrame(
             [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]],
             columns=['feature0', 'feature1', 'feature2', 'feature3', 'feature4', 'prediction']
@@ -52,19 +145,38 @@ with st.container():
             'home': sum([0.33, 0.45, 0.11, 0.89]),
             'away': sum([0.63, 0.95, 0.31, 0.79]),
         }
-        st.subheader('Features data and predictions')
-        col1, col2 = st.columns(2)
-        with col1:
-            st.metric(
-                label=f"{home} - Expected Goals (actual)",
-                value=f"{pred_goals['home']} ({actual_goals['home']})",
-                delta=pred_goals['home']-actual_goals['home']
-            )
-        with col2:
-            st.metric(
-                label=f"{away} - Expected Goals (actual)",
-                value=f"{pred_goals['away']} ({actual_goals['away']})",
-                delta=pred_goals['away']-actual_goals['away']
-            )
+        
 
         st.table(data=events)
+        
+
+
+
+
+#with st.container():
+#    form = st.form(key='GameID')
+#    gameid = form.number_input('Enter GameID', step=None, value=0)
+#    submit_button = form.form_submit_button(label='Ping game')
+#    # gameid = st.text_input('GameID')
+#
+#    if submit_button:
+#     if st.button('Ping game'):
+#
+#        data = requests.get('https://statsapi.web.nhl.com/api/v1/game/{}/feed/live/'.format(gameid)).json()
+#        print(data["liveData"]["plays"]["allPlays"])
+#        period_no = data["liveData"]["plays"]["allPlays"][-1]["about"]["period"]
+#        periodTimeRemaining = data["liveData"]["plays"]["allPlays"][-1]["about"]["periodTimeRemaining"]
+#        # td_home = data["liveData"]["boxscore"]["teams"]["home"]["teamStats"]["teamSkaterStats"]
+#        home = data["gameData"]["teams"]["home"]["name"]
+#        away = data["gameData"]["teams"]["away"]["name"]
+#        st.subheader(f'Game {gameid}: {away} @ {home}')
+#        st.write(f'Period {period_no} - {periodTimeRemaining} left')
+#        actual_goals = {
+#            'home': data["liveData"]["plays"]["allPlays"][-1]["about"]["goals"]["home"],
+#            'away': data["liveData"]["plays"]["allPlays"][-1]["about"]["goals"]["away"],
+#        }
+#
+#        # TODO get data from client API
+#        #  - Predictions
+#        #  - Features
+#
